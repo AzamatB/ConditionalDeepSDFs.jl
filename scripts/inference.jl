8aa665a8defeafe756479d5120a295aae650884d
Azamat Berdyshev <aberdysh@gmail.com>
2026-02-16 10:07:39 -0500

    fixes

--- scripts/inference.jl
+++ scripts/inference.jl
@@ -2,7 +2,8 @@ using Pkg
 Pkg.activate(@__DIR__)
 
 using ConditionalDeepSDFs
-using ConditionalDeepSDFs: ConditionalSDF, GridSlabs, LazyUnitCubeGrid, MeshSDFSampler
+using ConditionalDeepSDFs: ConditionalSDF, GridSlabs, LazyUnitCubeGrid, MeshSDFSampler,
+    construct_mesh, point_indices, slab_points, visualize
 using Reactant
 using JLD2
 using Lux
@@ -20,28 +21,28 @@ states = device(Lux.testmode(trained_model.states))
 display(model)
 
 # load the mesh samplers
-dataset_path = normpath(joinpath(@__DIR__, "../data/preprocessed/mesh_samplers_test.jld2"))
+dataset_path = normpath(joinpath(@__DIR__, "..", "data/preprocessed/mesh_samplers_test.jld2"))
 mesh_samplers = load_object(dataset_path)
 
 slab_size = 8   # or 4
 # compile the model
 mesh_sampler = first(mesh_samplers)
-grid_slabs = GridSlabs(mesh_sampler.resolution, slab_size)
+n = mesh_sampler.resolution
+# slab_size must evenly divide n
+grid_slabs = GridSlabs(n, slab_size)
 points = device(slab_points(grid_slabs, 1))
 mesh_params = device(mesh_sampler.parameters)
 model_compiled = @compile model((points, mesh_params), params, states)
 
 # run full inference on a single mesh
 mesh_params = device(mesh_sampler.parameters)
-n = mesh_sampler.resolution
-# slab_size must evenly divide n
-grid_slabs = GridSlabs(n, slab_size)
 sdf_flat = Array{Float32}(undef, n^3)
 for idx in eachindex(grid_slabs)
     points = device(slab_points(grid_slabs, idx))
     (signed_dists, _) = model_compiled((points, mesh_params), params, states)
+    signed_dists_cpu = cpu(signed_dists)
     indices = point_indices(grid_slabs, idx)
-    sdf_flat[indices] .= signed_dists
+    sdf_flat[indices] .= signed_dists_cpu
 end
 # reshape into a signed distance field over the grid
 sdf = reshape(sdf_flat, n, n, n)
